c57e57e83e3ead8f006da695fa856b91eed53d08
fahrezayy <fahrezafattan58@gamil.com>
2024-08-01 10:10:38 +0700

    vhhgu

--- Kalkulator.py
+++ Kalkulator.py
@@ -5,33 +5,28 @@ def kurang(x, y):
     return x - y
 
 def kali(x, y):
-    return x * y
-
+        return x * y
 def bagi(x, y):
     if y == 0:
         return "Tidak bisa membagi dengan nol!"
     return x / y
-
-# Menu untuk memilih operasi
+    # Menu untuk memilih operasi
 print("Pilih Operasi:")
 print("1. Tambah")
 print("2. Kurang")
 print("3. Kali")
 print("4. Bagi")
-
-# Input dari pengguna
+    # Input dari pengguna
 pilihan = input("Masukkan pilihan(1/2/3/4): ")
-
 angka1 = float(input("Masukkan angka pertama: "))
 angka2 = float(input("Masukkan angka kedua: "))
-
 if pilihan == '1':
-    print(f"Hasil: {angka1} + {angka2} = {tambah(angka1, angka2)}")
+    print(f"Hasil: {angka2} + {angka2} = {tambah(angka1, angka2)}")
 elif pilihan == '2':
-    print(f"Hasil: {angka1} - {angka2} = {kurang(angka1, angka2)}")
+    print(f"Hasil: {angka2} - {angka2} = {kurang(angka1, angka2)}")
 elif pilihan == '3':
-    print(f"Hasil: {angka1} * {angka2} = {kali(angka1, angka2)}")
+    print(f"Hasil: {angka2} * {angka2} = {kali(angka1, angka2)}")
 elif pilihan == '4':
-    print(f"Hasil: {angka1} / {angka2} = {bagi(angka1, angka2)}")
+    print(f"Hasil: {angka2} / {angka2} = {bagi(angka1, angka2)}")
 else:
     print("Pilihan tidak valid")
